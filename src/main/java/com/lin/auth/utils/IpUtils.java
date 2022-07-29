4c04275f37deb3d18a3cf35d33ae93c64540d03c
fengsulin <fengsu_lin@163.com>
2022-07-29 16:19:24 +0800

    整合ip2region

--- src/main/java/com/lin/auth/utils/IpUtils.java
+++ src/main/java/com/lin/auth/utils/IpUtils.java
@@ -1,16 +1,23 @@
 package com.lin.auth.utils;
 
 
+import org.lionsoul.ip2region.xdb.Searcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
 /**
- * 获取客户端ip工具类
+ * 获取客户端所在地工具类
  */
 public class IpUtils {
+
+    private final Logger logger = LoggerFactory.getLogger(IpUtils.class);
 
     /**
      * 获取ip地址
@@ -75,5 +82,92 @@ public class IpUtils {
             sb.append(s.length() == 1 ? 0 + s : s);
         }
         return sb.toString().trim().toUpperCase();
+    }
+
+    /**
+     * 完全基于文件查询
+     * @param dbPath：xdb文件所在路径
+     * @param ip：需要查询的ip
+     * @return String：返回ip所在地字符串
+     */
+    public String getIpOnlyFile(String dbPath,String ip) {
+        // 1、创建 searcher 对象
+        Searcher searcher = null;
+        String region = null;
+        try {
+            searcher = Searcher.newWithFileOnly(dbPath);
+            // 查询
+            region = searcher.search(ip);
+        } catch (IOException e) {
+
+            logger.error("failed to create searcher with `%s`: %s\n", dbPath, e);
+        } catch (Exception e) {
+            logger.info("failed to search(%s): %s\n", ip, e);
+        }
+        // 3、关闭资源
+        try {
+            searcher.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return region;
+        // 备注：并发使用，每个线程需要创建一个独立的 searcher 对象单独使用。
+    }
+
+    /**
+     * 缓存 VectorIndex 索引
+     * @param dbPath：xdb文件所在路径
+     * @param ip：需要查询的ip
+     * @return String：返回ip所在地字符串
+     */
+    public String getIpWithCache(String dbPath,String ip){
+        // 1、从 dbPath 中预先加载 VectorIndex 缓存，并且把这个得到的数据作为全局变量，后续反复使用。
+        byte[] vIndex = new byte[0];
+        // 2、使用全局的 vIndex 创建带 VectorIndex 缓存的查询对象。
+        Searcher searcher = null;
+        String region = null;
+        try {
+            vIndex = Searcher.loadVectorIndexFromFile(dbPath);
+            searcher = Searcher.newWithVectorIndex(dbPath, vIndex);
+            region = searcher.search(ip);
+        } catch (Exception e) {
+            logger.error("failed to load vector index from `%s`: %s\n", dbPath, e);
+        }
+
+        // 4、关闭资源
+        if(searcher != null){
+            try {
+                searcher.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return region;
+        // 备注：每个线程需要单独创建一个独立的 Searcher 对象，但是都共享全局的制度 vIndex 缓存。
+    }
+
+    /**
+     * 缓存整个sdb数据
+     * @param dbPath：xdb文件所在路径
+     * @param ip：需要查询的ip
+     * @return String：返回ip所在地字符串
+     */
+    public String getIpWithAllCache(String dbPath,String ip){
+        // 1、从 dbPath 加载整个 xdb 到内存。
+        byte[] cBuff;
+        // 2、使用上述的 cBuff 创建一个完全基于内存的查询对象。
+        Searcher searcher;
+        String region = null;
+        try {
+            cBuff = Searcher.loadContentFromFile(dbPath);
+            searcher = Searcher.newWithBuffer(cBuff);
+            region = searcher.search(ip);
+        } catch (Exception e) {
+            logger.error("failed to load content from `%s`: %s\n", dbPath, e);
+        }
+        return region;
+        // 4、关闭资源 - 该 searcher 对象可以安全用于并发，等整个服务关闭的时候再关闭 searcher
+        // searcher.close();
+        // 备注：并发使用，用整个 xdb 数据缓存创建的查询对象可以安全的用于并发，也就是你可以把这个 searcher 对象做成全局对象去跨线程访问。
     }
 }
